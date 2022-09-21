a3c2b265654aef8b362e9d35db75281b347a5868
Matthew-Mays <matthew.c.mays96@gmail.com>
2022-09-21 12:09:12 -0500

    add ability to add cards to quizzes

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
-import { addQuiz, selectTopics } from "../features/topics/topicsSlice";
+import { selectTopics } from "../features/topics/topicsSlice";
 import { useSelector, useDispatch } from "react-redux";
 import { addQuizForTopicId } from "../features/quizzes/quizzesSlice";
+import { addCard } from "../features/cards/cardsSlice";
 
 export default function NewQuizForm() {
   const [name, setName] = useState("");
@@ -23,9 +24,14 @@ export default function NewQuizForm() {
     const cardIds = [];
 
     // create the new cards here and add each card's id to cardIds
+    cards.forEach((card) => {
+      let cardId = uuidv4();
+      cardIds.push(cardId);
+      dispatch(addCard({...card, id: cardId}))
+    })
     // create the new quiz here
     let quizId = uuidv4();
-    
+
     dispatch(
       addQuizForTopicId({
         id: quizId,
